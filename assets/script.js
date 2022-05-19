9e329c5a777904e123e3bdeb87a6fa06563a4989
Althea Hanson <altheacodes@gmail.com>
2022-05-19 19:11:29 -0400

    generate password app completed

--- assets/script.js
+++ assets/script.js
@@ -18,14 +18,15 @@ function generatePassword() {
   //   password length 8 < 128
   console.log("if statement", passwordLength < 8 || passwordLength > 128);
   if (passwordLength < 8 || passwordLength > 128) {
-    generatePassword();
+    alert("Select a number between 8 and 128");
+    return;
   }
 
   //   lowercase, uppercase, numeric, and/or special characters
-  let promptLowerCase = window.confirm("Would you like to use small letters?");
+  let promptLowerCase = window.confirm("Would you like to use lower Case?");
   console.log("promptLowerCase", promptLowerCase);
 
-  let promptUpperCase = window.confirm("Would you like to use alpha letters?");
+  let promptUpperCase = window.confirm("Would you like to use upper Case");
   console.log("promptUpperCase", promptUpperCase);
 
   let promptNumbers = window.confirm("Would you like to use a number?");
@@ -43,19 +44,36 @@ function generatePassword() {
   if (promptUpperCase === true){
     allChar = allChar + upperCase;
   }
-  if(numbers === true){
+  if(promptNumbers === true){
     allChar = allChar + numbers;
   }
-  if(specialChar === true){
+  if(promptSpecialChar === true){
     allChar = allChar + specialChar;
+  }
+  console.log("no slection", promptLowerCase == false && promptUpperCase == false && promptNumbers == false && promptSpecialChar == false);
+
+  // check to ensure at least one criteria is selected
+  if (promptLowerCase == false && promptUpperCase == false && promptNumbers == false && promptSpecialChar == false) {
+    alert("At Least one criteria must be selected");
+    return;
+    
   }
 
   //3 Generate password based on selected criteria
+  var readyPassword = [];
+  var fakePassword = allChar.split("");
+  console.log(fakePassword);
+  for (i = 0; i < passwordLength; i++) {
+    var randomPassword = fakePassword[Math.floor(Math.random() * fakePassword.length)];
+    readyPassword.push(randomPassword);
+  }
+  console.log(randomPassword);
+  console.log(readyPassword);
 
 
 
   //4 display generated password on the page
-  return allChar;
+  return readyPassword.join("");
 }
 
 
