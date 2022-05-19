7579b7d2392ab3a92c4ee9f6d7d441e1474041b8
Althea Hanson <altheacodes@gmail.com>
2022-05-19 16:57:47 -0400

    added variable to collect all characters. validated te users selection

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,61 @@
 // declare variables
 let lowerCase = "abcdefghijklmnopqrstuvwxyz";
 let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let numbers = '0123456789';
-let specialChar = '!@#$%^&*=-_';
+let numbers = "0123456789";
+let specialChar = "!@#$%^&*=-_";
 
+let allChar = "";
 
 
 
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
-function generatePassword(){
+function generatePassword() {
   console.log("Thanks for clicking!")
-//1 Prompt the user for password criteria
-   let passwordLength = window.prompt("How many characters do you want in your password?");
-   console.log("passwordLength",passwordLength);
-   //   password length 8 < 128
-   console.log("if statement" ,passwordLength < 8 || passwordLength > 128 );
-   if (passwordLength < 8 || passwordLength > 128 ) {
-     passwordLength = window.prompt("Please enter charters between 8 and 128");
-     console.log("passwordlength", passwordLength );
-   }
+  //1 Prompt the user for password criteria
+  let passwordLength = window.prompt("How many characters do you want in your password?");
+  console.log("passwordLength", passwordLength);
+  //   password length 8 < 128
+  console.log("if statement", passwordLength < 8 || passwordLength > 128);
+  if (passwordLength < 8 || passwordLength > 128) {
+    generatePassword();
+  }
 
-//   lowercase, uppercase, numeric, and/or special characters
-   let promptLowerCase = window.prompt("Would you like to use small letters?");
-   console.log("promptLowerCase",promptLowerCase);
+  //   lowercase, uppercase, numeric, and/or special characters
+  let promptLowerCase = window.confirm("Would you like to use small letters?");
+  console.log("promptLowerCase", promptLowerCase);
 
-   let promptUpperCase = window.prompt("Would you like to use alpha letters?");
-   console.log("promptUpperCase",promptUpperCase);
+  let promptUpperCase = window.confirm("Would you like to use alpha letters?");
+  console.log("promptUpperCase", promptUpperCase);
 
-   let promptNumbers = window.prompt("Would you like to use a number?");
-   console.log("promptNumbers",promptNumbers);
+  let promptNumbers = window.confirm("Would you like to use a number?");
+  console.log("promptNumbers", promptNumbers);
 
-   let promptSpecialChar = window.prompt("Would you like to use special characters?");
-   console.log("promptSpecialChar",promptSpecialChar);
-
+  let promptSpecialChar = window.confirm("Would you like to use special characters?");
+  console.log("promptSpecialChar", promptSpecialChar);
 
 
+  //2 validate the input   
+
+  if (promptLowerCase === true) {
+    allChar = allChar + lowerCase;
+  }
+  if (promptUpperCase === true){
+    allChar = allChar + upperCase;
+  }
+  if(numbers === true){
+    allChar = allChar + numbers;
+  }
+  if(specialChar === true){
+    allChar = allChar + specialChar;
+  }
+
+  //3 Generate password based on selected criteria
 
 
-//2 validate the input   
 
-//3 Generate password based on selected criteria
-
-
-
-//4 display generated password on the page
-  return "Generated password goes here";
+  //4 display generated password on the page
+  return allChar;
 }
 
 
